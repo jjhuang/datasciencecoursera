528f13820837909a8e7cf0f1f6ffcdef19367763
Justin Huang <jhuang@bitglass.com>
2015-05-05 22:10:32 -0700

    Initial homework submission.

---
+++ plot1.R
@@ -0,0 +1,5 @@
+data <- read.table("../household_power_consumption.txt", sep=";",skip=66637,nrows=2881)
+names(data) <- unlist(read.table("./household_power_consumption.txt", sep=";",nrows=1))
+png(filename='plot1.png')
+hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power", breaks=12)
+dev.off()
